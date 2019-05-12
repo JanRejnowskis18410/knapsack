ac685b1e359cb7086e7914e4c69abb310439353b
sidletsky <oleksandr.sidletskyi@gmail.com>
2019-05-12 10:43:22 +0200

    calculating perfect knapsack on the fly

--- main.go
+++ main.go
@@ -2,75 +2,58 @@ package main
 
 import (
 	"fmt"
-	repo "github.com/sidletsky/knapsack/repository"
+	"github.com/sidletsky/knapsack/repository"
 	"math"
-	"sort"
 	"time"
 )
 
 type Knapsack struct {
 	TotalValue           int
 	TotalWeight          int
-	Items                []repo.Item
+	Items                []repository.Item
 	CharacteristicVector []int
 }
 
 func main() {
-	repository, err := repo.New("data/test1")
+	repo, err := repository.New("data/test2")
 	if err != nil {
 		panic(err)
 	}
 	startTime := time.Now()
-	characteristicVectors := createBinaryMatrix(repository.Size)
-	// building bags of items
-	knapsacks := make([]Knapsack, int(math.Pow(2, float64(repository.Size))))
-	for i, iv := range characteristicVectors {
-		knapsack := Knapsack{}
-		for i, jv := range iv {
-			if jv == 1 {
-				knapsack.TotalValue += repository.Items[i].Value
-				knapsack.TotalWeight += repository.Items[i].Weight
-				knapsack.Items = append(knapsack.Items, repository.Items[i])
-				knapsack.CharacteristicVector = iv
-			}
-		}
-		knapsacks[i] = knapsack
-	}
-	// sorting from the biggest to the smallest
-	sort.Slice(knapsacks, func(i, j int) bool {
-		return knapsacks[i].TotalValue > knapsacks[j].TotalValue
-	})
-
-	// searching for the perfect knapsack
-	fmt.Println("Knapsack's capacity:", repository.Capacity)
-	for _, v := range knapsacks {
-		if v.TotalWeight <= repository.Capacity {
-			fmt.Println("Characteristic vector:", v.CharacteristicVector)
-			fmt.Println("Total value:", v.TotalValue)
-			fmt.Println("Total weight:", v.TotalWeight)
-			fmt.Println("Items:")
-			for _, item := range v.Items {
-				fmt.Println("id:", item.Id, "weight:", item.Weight, "value:", item.Value)
-			}
-			break
-		}
-	}
-	duration := time.Since(startTime)
-	fmt.Println("Time took:", duration)
-}
-
-func createBinaryMatrix(size int) [][]int {
-	dimension := math.Pow(2, float64(size))
-	//dimension := int(math.Pow(float64(size), 2))
-	matrix := make([][]int, int(dimension))
-	for i := 0.; i < dimension; i++ {
-		vector := make([]int, size)
+	totalKnapsacks := math.Pow(2, float64(repo.Size))
+	maxKnapsack := Knapsack{}
+	for i := 0.; i < totalKnapsacks; i++ {
+		// create vector representation
+		vector := make([]int, repo.Size)
 		temp := i
-		for j := size - 1; j >= 0; j-- {
+		for j := repo.Size - 1; j >= 0; j-- {
 			vector[j] = int(math.Mod(temp, 2))
 			temp = math.Floor(temp / 2)
 		}
-		matrix[int(i)] = vector
+		// building knapsack with items
+		currentKnapsack := Knapsack{}
+		for i, v := range vector {
+			if v == 1 {
+				currentKnapsack.TotalValue += repo.Items[i].Value
+				currentKnapsack.TotalWeight += repo.Items[i].Weight
+				currentKnapsack.Items = append(currentKnapsack.Items, repo.Items[i])
+				currentKnapsack.CharacteristicVector = vector
+			}
+		}
+		// choosing max knapsack on the fly
+		if maxKnapsack.TotalValue < currentKnapsack.TotalValue && currentKnapsack.TotalWeight <= repo.Capacity {
+			maxKnapsack = currentKnapsack
+		}
 	}
-	return matrix
+	// perfect knapsack output
+	fmt.Println("Knapsack's capacity:", repo.Capacity)
+	fmt.Println("Characteristic vector:", maxKnapsack.CharacteristicVector)
+	fmt.Println("Total value:", maxKnapsack.TotalValue)
+	fmt.Println("Total weight:", maxKnapsack.TotalWeight)
+	fmt.Println("Items:")
+	for _, item := range maxKnapsack.Items {
+		fmt.Println("id:", item.Id, "weight:", item.Weight, "value:", item.Value)
+	}
+	duration := time.Since(startTime)
+	fmt.Println("Time took:", duration)
 }
